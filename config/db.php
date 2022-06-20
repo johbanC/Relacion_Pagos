a7a1dae108127e2335b2ac818381675a8cdf95d2
johbanC <66193457+johbanC@users.noreply.github.com>
2022-06-19 20:49:44 -0500

    Update db.php
    Elimine la clave de conexion para evitar daños a futuro

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
  
 class Database{
     public static function connect(){
-        $db = new mysqli('localhost', 'u845733158_desarrollo', 'Rosa.2106', 'u845733158_desarrollo');
+        $db = new mysqli('localhost', 'u845733158_desarrollo', '', 'u845733158_desarrollo');
         $db->query("SET NAMES 'uft8'");
         return $db;
     }
