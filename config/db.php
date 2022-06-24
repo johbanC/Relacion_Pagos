008d960191fe89aa9ff0c19b690238e3747182ff
johbanC <66193457+johbanC@users.noreply.github.com>
2022-06-24 08:17:03 -0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -8,31 +8,4 @@ class Database{
     }
 }
 
-
-/*
-class Database{
-    private static $db_host='localhost';
-
-    private static $db_user='u845733158_johbanc';
-
-    private static $db_pass='Rosa.2106';
-
-    private static $db_name='u845733158_app';
-
-    //using public static function to avoid overloading
-    public static function connect() {
-        $db=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
-        if($db){
-            echo "sorry";
-        }else {
-            echo "si";//return $db;
-        }
-    }
-}
-*/
-
-
-
-
-
 ?>
