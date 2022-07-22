3fd0dcaa2c3013c524c1edaad0f1c09afd5a45e8
Johban Clavijo <66193457+johbanC@users.noreply.github.com>
2022-07-22 01:03:13 -0500

    Add files via upload

--- controllers/RelacionPagoController.php
+++ controllers/RelacionPagoController.php
@@ -5,7 +5,31 @@ require_once 'models/RelacionPago.php';
 class RelacionPagoController{
 
 	public function index(){
-		echo "Index";
+		$relacionpago = new RelacionPago();
+		$relacionpago = $relacionpago->getAll();
+		require_once 'views/relacionpago/index.php';
+	}
+
+	/*DETALLES DE LA REALCION DE PAGO*/
+	public function detail(){
+
+		if (isset($_GET['id'])) {
+			$id = $_GET['id'];
+			$detail = true;
+			$relacionpago = new RelacionPago();
+			$relacionpago->setIdRelacionPago($id);
+			$relacionpago = $relacionpago->getOneDetailRelacionPago();
+			
+			/*de otro bloque
+			$propiedadP = new Propietario();
+			$propiedadP->setIdPropietario($id);
+			$propiedadP = $propiedadP->getOneDetailPropi();*/
+
+			require_once 'views/propietario/detail.php';			
+		}else{
+			header('Location:'.base_url.'Propietario/index');
+		}
+
 	}
 
 	public function create(){
@@ -46,7 +70,7 @@ class RelacionPagoController{
 
 				if ($saveRP) {
 					$_SESSION['relacionpago'] = 'complete';
-				
+
 
 					require_once 'models/DetalleRelacion.php';
 
@@ -59,7 +83,7 @@ class RelacionPagoController{
 					$iva = isset($_POST['iva']) ? $_POST['iva'] : false;
 					$total_agencia = isset($_POST['total_agencia']) ? $_POST['total_agencia'] : false;
 					$valor_consignado = isset($_POST['valor_consignado']) ? $_POST['valor_consignado'] : false;
-
+					
 					if ($mes) {
 						$Drelacionpago = new DetalleRelacion();
 						$Drelacionpago->setIdrelacionpago($idrelacionpago);
@@ -71,6 +95,24 @@ class RelacionPagoController{
 						$Drelacionpago->setIva($iva);
 						$Drelacionpago->setTotal_agencia($total_agencia);
 						$Drelacionpago->setValor_consignado($valor_consignado);
+
+						// GUARDAR ARCHIVO "DOCUMENTO FRONTAL"
+						if(isset($_FILES['recibopago'])){
+							$file = $_FILES['recibopago'];
+							$filename = $file['name'];
+							$mimetype = $file['type'];
+
+							$filename = str_replace(" ","-","$filename");
+							//NO FUNCIONO EL CAMBIO DE NOMBRE
+							//$nombrearchivo = rename($filename, $idPropietario."_".$nombre_1."-".$apellido_1."-DocFrontal".$mimetype);	
+
+							if(!is_dir('librerias/dist/recibopago/relaciondepago/'.$nro_relacion)){
+								mkdir('librerias/dist/recibopago/relaciondepago/'.$nro_relacion, 0777, true);
+							}
+
+							$Drelacionpago->setImg($filename);
+							move_uploaded_file($file['tmp_name'], 'librerias/dist/recibopago/relaciondepago/'.$nro_relacion.'/'.$filename);							
+						}
 
 						/*GUARDAR LOS DETALLE DE LA RELACION DE PAGO*/
 						$saveDRP = $Drelacionpago->saveDRP();
@@ -98,10 +140,29 @@ class RelacionPagoController{
 							$savePR = $Prelacionpago->savePR();
 						}
 
+						if ($savePR) {
+
+							$_SESSION['Drelacionpago'] = 'complete';
+
+
+							/*DEDUCCIONES DE LA RELACION DE PAGOS*/
+							require_once 'models/DeduccionesRelacion.php';
+							$idrelacionpago = isset($_POST['idRelacionPago']) ? $_POST['idRelacionPago'] : false;
+							$fecha_d = isset($_POST['fecha_d']) ? $_POST['fecha_d'] : false;
+							$descripcion_d = isset($_POST['descripcion_d']) ? $_POST['descripcion_d'] : false;
+							$valor_d = isset($_POST['valor_d']) ? $_POST['valor_d'] : false;
+
+							if ($fecha_d) {
+								$Drelacionpago = new DeduccionesRelacion();
+								$Drelacionpago->setIdrelacionpago($idrelacionpago);
+								$Drelacionpago->setfecha($fecha_d);
+								$Drelacionpago->setDescripcion($descripcion_d);
+								$Drelacionpago->setValor($valor_d);
+
+								$saveDR = $Drelacionpago->saveDR();
+							}
+						}
 					}
-
-
-
 
 				}else{
 					$_SESSION['relacionpago'] = 'failed';
@@ -112,6 +173,7 @@ class RelacionPagoController{
 		}else{
 			$_SESSION['relacionpago'] = 'failed';
 		}
+		exit();
 		echo("<script>location.href = '".base_url."Propietario/detail&id=".$idpropietario."';</script>");
 	}
 
